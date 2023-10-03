a019d83c0919e4baf2ced92928dfda7f82807e73
Robert Phillips <rphillips11995@gmail.com>
2023-10-03 17:09:31 -0400

    change GWAS included and alter paths

--- MAGMA/mouse/magma_step1_annotate_mouse_homologs.sh
+++ MAGMA/mouse/magma_step1_annotate_mouse_homologs.sh
@@ -21,9 +21,9 @@ date
 module load magma/1.10
 
 #Variables for specific paths. 
-ANNO=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/mouse/mouse_expressing_hg19_homs.gene.loc
-SNPDATA=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data
-MOUSE=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/mouse
+ANNO_Hg19=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/mouse_analysis/mouse_expressing_hg19_homs.gene.loc
+ANNO_Hg38=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/mouse_analysis/mouse_expressing_hg38_homs.gene.loc
+MOUSE=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/mouse_analysis/magma_output
 
 #Variables for .snploc files.
 AD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/AD
@@ -31,40 +31,34 @@ ADHD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profil
 ASD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/ASD
 BIP=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/BIP
 CUD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/CUD
-OUD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/OUD
-PD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/PD
 SCZ=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/SCZ
+OUD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/OUD
+MDD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/MDD
 
 ## Step 1 - Annotation (SNP : gene mapping)
-#ADHD
-magma --annotate window=35,10 --snp-loc $ADHD/adhd_jul2017.snploc --gene-loc $ANNO --out $MOUSE/adhd_jul2017_mouse_homologs
-
 #Alzheimers
-magma --annotate window=35,10 --snp-loc $AD/AD_sumstats_Jansenetal_2019sept.snploc --gene-loc $ANNO --out $MOUSE/AD_Jansenetal_2019sept_mouse_homologs
+magma --annotate window=35,10 --snp-loc $AD/AD_sumstats_Jansenetal_2019sept.snploc --gene-loc $ANNO_Hg19 --out $MOUSE/AD_Jansenetal_2019sept_mouse_homologs
+
+#ADHD
+magma --annotate window=35,10 --snp-loc $ADHD/adhd_jul2017.snploc --gene-loc $ANNO_Hg19 --out $MOUSE/adhd_jul2017_mouse_homologs
 
 #ASD
-magma --annotate window=35,10 --snp-loc $ASD/iPSYCH-PGC_ASD_Nov2017.snploc --gene-loc $ANNO --out $MOUSE/iPSYCH-PGC_ASD_Nov2017_mouse_homologs
+magma --annotate window=35,10 --snp-loc $ASD/iPSYCH-PGC_ASD_Nov2017.snploc --gene-loc $ANNO_Hg19 --out $MOUSE/iPSYCH-PGC_ASD_Nov2017_mouse_homologs
 
 #BIP
-magma --annotate window=35,10 --snp-loc $BIP/PGC_BIP32b.snploc --gene-loc $ANNO --out $MOUSE/BIP_PGC_2018_mouse_homologs
+magma --annotate window=35,10 --snp-loc $BIP/PGC_BIP32b.snploc --gene-loc $ANNO_Hg19 --out $MOUSE/BIP_PGC_2018_mouse_homologs
 
 #CUD
-magma --annotate window=35,10 --snp-loc $CUD/CUD_GWAS_iPSYCH_June2019.snploc --gene-loc $ANNO --out $MOUSE/CUD_iPSYCH_June2019_mouse_homologs
-
-#PD
-magma --annotate window=35,10 --snp-loc $PD/nallsetal2019_PD.snploc --gene-loc $ANNO --out $MOUSE/PD_nallsetal2019_mouse_homologs
+magma --annotate window=35,10 --snp-loc $CUD/CUD_GWAS_iPSYCH_June2019.snploc --gene-loc $ANNO_Hg19 --out $MOUSE/CUD_iPSYCH_June2019_mouse_homologs
 
 #SCZ
-magma --annotate window=35,10 --snp-loc $SCZ/PGC_SCZ.snploc --gene-loc $ANNO --out $MOUSE/PGC_SCZ_mouse_homologs
+magma --annotate window=35,10 --snp-loc $SCZ/PGC3_SCZ_wave3.european.autosome.public.v3.snploc --gene-loc $ANNO_Hg19 --out $MOUSE/PGC_SCZ_mouse_homologs
 
-#OpioidUseDisorder
-magma --annotate window=35,10 --snp-loc $OUD/ODvsEx_AA_mapped.snploc --gene-loc $ANNO --out $MOUSE/ODvsEx_AA_mouse_homologs
-magma --annotate window=35,10 --snp-loc $OUD/ODvsEx_EA_mapped.snploc --gene-loc $ANNO --out $MOUSE/ODvsEx_EA_mapped_homologs
-magma --annotate window=35,10 --snp-loc $OUD/ODvsEx_mapped.snploc --gene-loc $ANNO --out $MOUSE/ODvsEx_mapped_homologs
-magma --annotate window=35,10 --snp-loc $OUD/ODvsUn_AA_mapped.snploc --gene-loc $ANNO --out $MOUSE/ODvsUn_AA_mapped_homologs
-magma --annotate window=35,10 --snp-loc $OUD/ODvsUn_EA_mapped.snploc --gene-loc $ANNO --out $MOUSE/ODvsUn_EA_mapped_homologs
-magma --annotate window=35,10 --snp-loc $OUD/ODvsUn_mapped.snploc --gene-loc $ANNO --out $MOUSE/ODvsUn_mapped_homologs
+#MDD 
+magma --annotate window=35,10 --snp-loc $MDD/MDD.phs001672.pha005122.snploc --gene-loc $ANNO_Hg38 --out $MOUSE/MDD_pha005122_mouse_homologs
 
+#OUD 
+magma --annotate window=35,10 --snp-loc $OUD/OUD.phs001672.pha004954.snploc --gene-loc $ANNO_Hg38 --out $MOUSE/OUD_pha004954_mouse_homologs
 
 echo "**** Job ends ****"
 date
