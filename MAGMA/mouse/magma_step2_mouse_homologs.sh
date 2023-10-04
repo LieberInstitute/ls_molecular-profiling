8189069db9552d3943bbc68efab927f32c71a323
Robert Phillips <rphillips11995@gmail.com>
2023-10-04 17:29:07 -0400

    update GWAS and change gene model to snp-wise

--- MAGMA/mouse/magma_step2_mouse_homologs.sh
+++ MAGMA/mouse/magma_step2_mouse_homologs.sh
@@ -22,49 +22,43 @@ module load magma/1.10
 
 #Variables for paths and such
 BFILE=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/g1000_eur/g1000_eur
-ANNO=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/mouse/mouse_expressing_hg19_homs.gene.loc
-SNPDATA=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data
-MOUSE=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/mouse
+MOUSE=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/mouse_analysis/magma_output
+model="snp-wise"
 
-#Variables for .snploc files.
+#Variables for disorder/disease specific files containing all SNPs. 
 AD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/AD
 ADHD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/ADHD
 ASD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/ASD
 BIP=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/BIP
 CUD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/CUD
-OUD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/OUD
-PD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/PD
 SCZ=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/SCZ
+OUD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/OUD
+MDD=/dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/MAGMA/SNP_Data/GWAS_Tables/MDD
 
-## Step 2 - Gene analysis (from SNP-wise summary stats)
+## Step 2 - Gene analysis (from SNP-wise summary stats) 
 #AD
-magma --bfile $BFILE --gene-annot $MOUSE/AD_Jansenetal_2019sept_mouse_homologs.genes.annot --pval $AD/AD_sumstats_Jansenetal_2019sept.txt use=SNP,P ncol=Nsum --out $AD/AD
+magma --bfile $BFILE --gene-annot $MOUSE/AD_Jansenetal_2019sept_mouse_homologs.genes.annot --pval $AD/AD_sumstats_Jansenetal_2019sept.txt use=SNP,P ncol=Nsum --gene-model ${model} --out $AD/AD
 
 #ADHD
-magma --bfile $BFILE --gene-annot $MOUSE/adhd_jul2017_mouse_homologs.genes.annot --pval $ADHD/adhd_jul2017.txt use=SNP,P N=55374 --out $ADHD/adhd
+magma --bfile $BFILE --gene-annot $MOUSE/adhd_jul2017_mouse_homologs.genes.annot --pval $ADHD/adhd_jul2017.txt use=SNP,P N=55374 --gene-model ${model} --out $ADHD/adhd
 
 #ASD
-magma --bfile $BFILE --gene-annot $MOUSE/iPSYCH-PGC_ASD_Nov2017_mouse_homologs.genes.annot --pval $ASD/iPSYCH-PGC_ASD_Nov2017.txt use=SNP,P N=46351 --out $ASD/ASD
+magma --bfile $BFILE --gene-annot $MOUSE/iPSYCH-PGC_ASD_Nov2017_mouse_homologs.genes.annot --pval $ASD/iPSYCH-PGC_ASD_Nov2017.txt use=SNP,P N=46350 --gene-model ${model} --out $ASD/ASD
 
 #BIP
-magma --bfile $BFILE --gene-annot $MOUSE/BIP_PGC_2018_mouse_homologs.genes.annot --pval $BIP/PGC_BIP32b_withN.txt use=SNP,P ncol=Nca_Plus_Nco --out $BIP/BIP
+magma --bfile $BFILE --gene-annot $MOUSE/BIP_PGC_2018_mouse_homologs.genes.annot --pval $BIP/PGC_BIP32b_withN.txt use=SNP,P ncol=Nca_Plus_Nco --gene-model ${model} --out $BIP/BIP
 
 #CUD
-magma --bfile $BFILE --gene-annot $MOUSE/CUD_iPSYCH_June2019_mouse_homologs.genes.annot --pval $CUD/CUD_GWAS_iPSYCH_June2019.txt use=SNP,P N=51372 --out $CUD/CUD
-
-#OUD
-magma --bfile $BFILE --gene-annot $MOUSE/ODvsEx_AA_mouse_homologs.genes.annot --pval $OUD/OD_cases_vs._opioid-exposed_controls_in_African-ancestry_cohorts.txt use=rsID,P-Value ncol=Total_N  --out $OUD/ODvsEX_AA
-magma --bfile $BFILE --gene-annot $MOUSE/ODvsEx_EA_mapped_homologs.genes.annot --pval $OUD/OD_cases_vs._opioid-exposed_controls_in_European-ancestry_cohorts.txt use=rsID,P-Value ncol=Total_N  --out $OUD/ODvsEX_EA
-magma --bfile $BFILE --gene-annot $MOUSE/ODvsEx_mapped_homologs.genes.annot --pval $OUD/OD_cases_vs._opioid-exposed_controls_in_the_trans-ancestry_meta-analysis.txt use=rsID,P-Value ncol=Total_N  --out $OUD/ODvsEX_Trans
-magma --bfile $BFILE --gene-annot $MOUSE/ODvsUn_AA_mapped_homologs.genes.annot --pval $OUD/OD_cases_vs._opioid-unexposed_controls_in_African-ancestry_cohorts.txt  use=rsID,P-Value ncol=Total_N  --out $OUD/ODvsUN_AA
-magma --bfile $BFILE --gene-annot $MOUSE/ODvsUn_EA_mapped_homologs.genes.annot --pval $OUD/OD_cases_vs._opioid-unexposed_controls_in_European-ancestry_cohorts.txt   use=rsID,P-Value ncol=Total_N  --out $OUD/ODvsUN_EA
-magma --bfile $BFILE --gene-annot $MOUSE/ODvsUn_mapped_homologs.genes.annot --pval $OUD/OD_cases_vs._opioid-unexposed_controls_in_the_trans-ancestry_meta-analysis.txt  use=rsID,P-Value ncol=Total_N  --out $OUD/ODvsUN_Trans
-
-#PD
-magma --bfile $BFILE --gene-annot $MOUSE/PD_nallsetal2019_mouse_homologs.genes.annot --pval $PD/nallsetal2019_mapped.txt use=ID,p ncol=N --out $PD/PD
+magma --bfile $BFILE --gene-annot $MOUSE/CUD_iPSYCH_June2019_mouse_homologs.genes.annot --pval $CUD/CUD_GWAS_iPSYCH_June2019.txt use=SNP,P N=51372 --gene-model ${model} --out $CUD/CUD
 
 #SCZ
-magma --bfile $BFILE --gene-annot $MOUSE/PGC_SCZ_mouse_homologs.genes.annot --pval $SCZ/rall.txt use=snpid,p N=150064 --out $SCZ/SCZ
+magma --bfile $BFILE --gene-annot $MOUSE/PGC_SCZ_mouse_homologs.genes.annot --pval $SCZ/PGC3_SCZ_wave3.european.autosome.public.v3.pval ncol=N --gene-model ${model} --out $SCZ/SCZ
+
+#MDD
+magma --bfile $BFILE --gene-annot $MOUSE/MDD_pha005122_mouse_homologs.genes.annot --pval $MDD/MDD.phs001672.pha005122.pval N=1154267 --gene-model ${model} --out $MDD/MDD
+
+#OUD
+magma --bfile $BFILE --gene-annot $MOUSE/OUD_pha004954_mouse_homologs.genes.annot --pval $OUD/OUD.phs001672.pha004954.txt N=639063 --gene-model ${model} --out $OUD/OUD
 
 echo "**** Job ends ****"
 date
