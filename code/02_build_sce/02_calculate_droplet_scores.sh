6c72badd57f436fd41e68bf6cec8fa5f754a6093
Robert Phillips <rphillips11995@gmail.com>
2023-10-12 15:31:33 -0400

    shell script for emptyDrops and QC plots

---
+++ code/02_build_sce/02_calculate_droplet_scores.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#SBATCH --job-name=emptyDrops
+#SBATCH --output=emptyDrops_dropletQC.out
+#SBATCH --error=mptyDrops_dropletQC.err
+#
+# Number of CPUs allocated to each task.
+#SBATCH --cpus-per-task=5
+#
+# Mimimum memory required per allocated  CPU
+#SBATCH --mem-per-cpu=20G
+#
+# Send mail to the email address when the job fails
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robert.phillips@libd.org
+
+
+echo "********* Job Starts *********"
+date
+
+#load R
+module load conda_R/4.3
+
+#list modules for reproducibility purposes
+module list
+
+#run the Rjob
+Rscript 02_calculate_droplet_scores.R
+
+echo "********* Job Ends *********"
+date
