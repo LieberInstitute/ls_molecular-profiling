f9a83601af8484fa080425d09984da852b222c6f
Robert Phillips <rphillips11995@gmail.com>
2023-10-14 00:23:38 -0400

    Run GLM PCA and UMAP without MNN

--- code/02_build_sce/04_FeatureSelection_DimReduction.R
+++ code/02_build_sce/04_FeatureSelection_DimReduction.R
@@ -39,13 +39,91 @@ abline(v = 2000,lty = 2, col = "red")
 dev.off()
 
 #2000 should be good. 
+sce <- nullResiduals(sce,
+                     assay = "counts", 
+                     fam   = "binomial", 
+                     type  = "deviance",
+                     batch = as.factor(sce$Sample))
+# In addition: Warning messages:
+#     1: In asMethod(object) :
+#     sparse->dense coercion: allocating vector of size 1.0 GiB
+# 2: In asMethod(object) :
+#     sparse->dense coercion: allocating vector of size 1.0 GiB
+# 3: In .sparse2dense(x) :
+#     sparse->dense coercion: allocating vector of size 1.0 GiB
+# 4: In asMethod(object) :
+#     sparse->dense coercion: allocating vector of size 1.0 GiB
+# 5: In asMethod(object) :
+#     sparse->dense coercion: allocating vector of size 1.0 GiB
+# 6: In sqrt(x@x) : NaNs produced
+#Not sure about warnings. sparse-->dense coercion doesn't seem to affect function. 
+#No binomical deviances are NAs and  #ull residual matrix seems to be alright. 
+#Need to figure out why NANs are being produced and why this could be happening? 
+#In the meantime, all values are accounted for. Will move forward. 
 
 
-Sys.time()
-#[1] "2023-10-13 17:06:44 EDT"
-sce.ls <- nullResiduals(sce,
-                        assay = "counts", 
-                        fam   = "binomial", 
-                        type  = "deviance")
-Sys.time()
+#Take top 2000 highly deviant genes
+hdgs <- rownames(sce)[order(rowData(sce)$binomial_deviance, decreasing = T)][1:2000]
+hdgs.symbols <- rowData(sce)$gene_name[match(hdgs, rowData(sce)$gene_id)]
+
+#Run PCA
+sce_uncorrected <- runPCA(sce,
+                          exprs_values = "binomial_deviance_residuals",
+                          subset_row = hdgs, 
+                          ncomponents = 100,
+                          name = "GLMPCA_approx")
+
+# UMAP
+set.seed(1234)
+sce_uncorrected <- runUMAP(sce_uncorrected,
+                           dimred = "GLMPCA_approx",
+                           n_dimred = 50, 
+                           name = "UMAP")
+# t-SNE
+set.seed(1234)
+sce_uncorrected <- runTSNE(sce_uncorrected,
+                           dimred = "GLMPCA_approx",
+                           n_dimred = 50, 
+                           name = "TSNE")
+
+#PCA plot of top 10 PCs
+PCA_plots <- plotReducedDim(sce_uncorrected,
+               dimred = "GLMPCA_approx", 
+               colour_by = "Sample",
+               ncomponents = 6, 
+               point_alpha = 0.3)
+ggsave(PCA_plots,filename = here("plots","Dim_Red","multi_PCAs.png"))
+
+# UMAPs
+#Colored by sample, library size, and doublet score
+#Sample
+sample_umap <- plotReducedDim(sce_uncorrected,
+                              dimred = "UMAP", 
+                              colour_by = "Sample",
+                              point_alpha = 0.3)
+ggsave(sample_umap,filename = here("plots","Dim_Red","sample_umap.png"))
+
+#library size
+sum_umap <- plotReducedDim(sce_uncorrected,
+               dimred = "UMAP", colour_by = "sum",
+               point_alpha = 0.3)
+ggsave(sum_umap,filename = here("plots","Dim_Red","sum_umap.png"))
+
+#Doublet score
+doublet_umap <- plotReducedDim(sce_uncorrected,
+                               dimred = "UMAP", 
+                               colour_by = "doubletScore")
+ggsave(doublet_umap,filename = here("plots","Dim_Red","doublet_umap.png"))
+
+# TSNE by sample
+sample_TSNE <- plotReducedDim(sce_uncorrected,
+                              dimred = "TSNE", 
+                              colour_by = "Sample")
+ggsave(sample_TSNE,filename = here("plots","Dim_Red","sample_TSNE.png"))
+
+#batch effect results in cluster coming from a single sample. 
+#Will need to run MNN to fix this. 
+#save uncorrected object. 
+save(sce_uncorrected,file = here("processed-data","sce_uncorrected.rda"))
+
 
