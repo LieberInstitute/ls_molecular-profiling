b5c801c8fba511d5f4b4f51f732cf546dbeb6db4
Robert Phillips <rphillips11995@gmail.com>
2023-10-18 11:06:27 -0400

    plot umaps for different values

--- code/02_build_sce/04_FeatureSelection_DimReduction.R
+++ code/02_build_sce/04_FeatureSelection_DimReduction.R
@@ -96,61 +96,25 @@ ggsave(PCA_plots,filename = here("plots","Dim_Red","multi_PCAs_numericCutoff.png
 # UMAP
 #With testing 15,20,25,and 50
 #50 dimensions as in Tran, Maynard et al Neuron
-set.seed(1234)
-sce_uncorrected <- runUMAP(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 15, 
-                           name = "UMAP_15")
-#with 20 dimensions
-set.seed(1234)
-sce_uncorrected <- runUMAP(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 20, 
-                           name = "UMAP_20")
-
-#with 25 dimensions
-set.seed(1234)
-sce_uncorrected <- runUMAP(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 25, 
-                           name = "UMAP_25")
-
-#with 50 dimensions
-set.seed(1234)
-sce_uncorrected <- runUMAP(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 50, 
-                           name = "UMAP_50")
-
+for(i in c(15,20,25,50)){
+    print(i)
+    set.seed(1234)
+    sce_uncorrected <- runUMAP(sce_uncorrected,
+                               dimred = "GLMPCA_approx",
+                               n_dimred = i,
+                               name = paste0("UMAP_",i))
+}
 
 # t-SNE
 #with 15 dimensions
-set.seed(1234)
-sce_uncorrected <- runTSNE(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 15, 
-                           name = "TSNE_15")
-
-#with 20 dimensions
-set.seed(1234)
-sce_uncorrected <- runTSNE(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 20, 
-                           name = "TSNE_20")
-
-#with 25 dimensions
-set.seed(1234)
-sce_uncorrected <- runTSNE(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 25, 
-                           name = "TSNE_25")
-
-#with 50 dimensions
-set.seed(1234)
-sce_uncorrected <- runTSNE(sce_uncorrected,
-                           dimred = "GLMPCA_approx",
-                           n_dimred = 50, 
-                           name = "TSNE_50")
+for(i in c(15,20,25,50)){
+    print(i)
+    set.seed(1234)
+    sce_uncorrected <- runTSNE(sce_uncorrected,
+                               dimred = "GLMPCA_approx",
+                               n_dimred = i,
+                               name = paste0("TSNE_",i))
+}
 
 # UMAPs
 #Colored by sample, library size, and doublet score
@@ -227,56 +191,71 @@ reducedDim(sce_uncorrected,"mnn") <- glmpca_mnn$corrected
 sce <- sce_uncorrected
 rm(sce_uncorrected)
 
-#Rerun umpa and tsne
-set.seed(1234)
-
 #umap with 15,20,25,50 dimensions
+#Only going to run umap at this point. 
 for(i in c(15,20,25,50)){
+    set.seed(1234)
     sce <- runUMAP(sce,
                    dimred = "mnn",
                    name = paste0("UMAP_mnn_",i))
 }
 
 
-# pdf(here("plots","UMAP_corrected_by_sample.pdf"))
-# plotReducedDim(sce,
-#                dimred = "UMAP_mnn", 
-#                colour_by = "Sample")
-# dev.off()
-pdf(here("plots","Dim_Red","UMAP_corrected_by_sample_numericCutoffs.pdf"))
-plotReducedDim(sce,
-               dimred = "UMAP_mnn", 
-               colour_by = "Sample")
-dev.off()
+#Plot umaps by sample, sum, detected, and subsets_mito_percent
+#Sample
+for(i in c(15,20,25,50)){
+    x <- plotReducedDim(sce_uncorrected,
+                        dimred = paste0("UMAP_",i), 
+                        colour_by = "Sample",
+                        point_alpha = 0.3) +
+        ggtitle(paste(i,"Dimensions")) +
+        theme(plot.title = element_text(hjust = 0.5))
+    ggsave(x,filename = here("plots",
+                             "Dim_Red",
+                             paste0("sample_umap_numericCutoffs_",i,"dims_postMNN.png")))
+}
 
-# pdf(here("plots","Dim_Red","UMAP_corrected_mito.pdf"))
-# plotReducedDim(sce,
-#                dimred = "UMAP_mnn", colour_by = "subsets_Mito_percent",
-#                point_alpha = 0.3)
-# dev.off()
-pdf(here("plots","Dim_Red","UMAP_corrected_mito_numericCutoffs.pdf"))
-plotReducedDim(sce,
-               dimred = "UMAP_mnn", colour_by = "subsets_Mito_percent",
-               point_alpha = 0.3)
-dev.off()
+#sum
+for(i in c(15,20,25,50)){
+    x <- plotReducedDim(sce_uncorrected,
+                        dimred = paste0("UMAP_",i), 
+                        colour_by = "sum",
+                        point_alpha = 0.3) +
+        ggtitle(paste(i,"Dimensions")) +
+        theme(plot.title = element_text(hjust = 0.5))
+    ggsave(x,filename = here("plots",
+                             "Dim_Red",
+                             paste0("sum_umap_numericCutoffs_",i,"dims_postMNN.png")))
+}
 
-# pdf(here("plots","Dim_Red","UMAP_corrected_sum.pdf"))
-# plotReducedDim(sce,
-#                dimred = "UMAP_mnn", colour_by = "sum",
-#                point_alpha = 0.3)
-# dev.off()
-pdf(here("plots","Dim_Red","UMAP_corrected_sum_numericCutoffs.pdf"))
-plotReducedDim(sce,
-               dimred = "UMAP_mnn", colour_by = "sum",
-               point_alpha = 0.3)
-dev.off()
+#detected
+for(i in c(15,20,25,50)){
+    x <- plotReducedDim(sce_uncorrected,
+                        dimred = paste0("UMAP_",i), 
+                        colour_by = "detected",
+                        point_alpha = 0.3) +
+        ggtitle(paste(i,"Dimensions")) +
+        theme(plot.title = element_text(hjust = 0.5))
+    ggsave(x,filename = here("plots",
+                             "Dim_Red",
+                             paste0("detected_umap_numericCutoffs_",i,"dims_postMNN.png")))
+}
+
+#subsets mito percent
+for(i in c(15,20,25,50)){
+    x <- plotReducedDim(sce_uncorrected,
+                        dimred = paste0("UMAP_",i), 
+                        colour_by = "detected",
+                        point_alpha = 0.3) +
+        ggtitle(paste(i,"Dimensions")) +
+        theme(plot.title = element_text(hjust = 0.5))
+    ggsave(x,filename = here("plots",
+                             "Dim_Red",
+                             paste0("detected_umap_numericCutoffs_",i,"dims_postMNN.png")))
+}
 
 
-pdf(here("plots","Dim_Red","UMAP_corrected_detected_numericCutoffs.pdf"))
-plotReducedDim(sce,
-               dimred = "UMAP_mnn", colour_by = "detected",
-               point_alpha = 0.3)
-dev.off()
+
 
 #One cluster is dominated by Sample 1. Checking expression values to identify if this is a 
 #batch correction issue, or if this is due to the 
@@ -320,118 +299,3 @@ Sys.time()
 proc.time()
 options(width = 120)
 session_info()
-# [1] "Reproducibility information:"
-# [1] "2023-10-16 17:16:43 EDT"
-#     user   system  elapsed 
-# 1024.822   90.870 7503.115 
-# ─ Session info ─────────────────────────────────────────────────────────────────────
-# setting  value
-# version  R version 4.3.1 Patched (2023-07-19 r84711)
-# os       Rocky Linux 9.2 (Blue Onyx)
-# system   x86_64, linux-gnu
-# ui       X11
-# language (EN)
-# collate  en_US.UTF-8
-# ctype    en_US.UTF-8
-# tz       US/Eastern
-# date     2023-10-16
-# pandoc   3.1.3 @ /jhpce/shared/community/core/conda_R/4.3/bin/pandoc
-# 
-# ─ Packages ─────────────────────────────────────────────────────────────────────────
-# package              * version   date (UTC) lib source
-# abind                  1.4-5     2016-07-21 [2] CRAN (R 4.3.1)
-# batchelor              1.16.0    2023-04-25 [2] Bioconductor
-# beachmat               2.16.0    2023-04-25 [2] Bioconductor
-# beeswarm               0.4.0     2021-06-01 [2] CRAN (R 4.3.1)
-# Biobase              * 2.60.0    2023-04-25 [2] Bioconductor
-# BiocGenerics         * 0.46.0    2023-04-25 [2] Bioconductor
-# BiocNeighbors          1.18.0    2023-04-25 [2] Bioconductor
-# BiocParallel           1.34.2    2023-05-22 [2] Bioconductor
-# BiocSingular           1.16.0    2023-04-25 [2] Bioconductor
-# bitops                 1.0-7     2021-04-24 [2] CRAN (R 4.3.1)
-# bluster                1.10.0    2023-04-25 [2] Bioconductor
-# cli                    3.6.1     2023-03-23 [2] CRAN (R 4.3.1)
-# cluster                2.1.4     2022-08-22 [3] CRAN (R 4.3.1)
-# codetools              0.2-19    2023-02-01 [3] CRAN (R 4.3.1)
-# colorout             * 1.2-2     2023-09-22 [1] Github (jalvesaq/colorout@79931fd)
-# colorspace             2.1-0     2023-01-23 [2] CRAN (R 4.3.1)
-# cowplot                1.1.1     2020-12-30 [2] CRAN (R 4.3.1)
-# crayon                 1.5.2     2022-09-29 [2] CRAN (R 4.3.1)
-# DelayedArray           0.26.7    2023-07-28 [2] Bioconductor
-# DelayedMatrixStats     1.22.6    2023-08-28 [2] Bioconductor
-# dplyr                  1.1.3     2023-09-03 [2] CRAN (R 4.3.1)
-# dqrng                  0.3.1     2023-08-30 [2] CRAN (R 4.3.1)
-# edgeR                  3.42.4    2023-05-31 [2] Bioconductor
-# fansi                  1.0.4     2023-01-22 [2] CRAN (R 4.3.1)
-# farver                 2.1.1     2022-07-06 [2] CRAN (R 4.3.1)
-# generics               0.1.3     2022-07-05 [2] CRAN (R 4.3.1)
-# GenomeInfoDb         * 1.36.3    2023-09-07 [2] Bioconductor
-# GenomeInfoDbData       1.2.10    2023-07-20 [2] Bioconductor
-# GenomicRanges        * 1.52.0    2023-04-25 [2] Bioconductor
-# ggbeeswarm             0.7.2     2023-04-29 [2] CRAN (R 4.3.1)
-# ggplot2              * 3.4.3     2023-08-14 [2] CRAN (R 4.3.1)
-# ggrepel                0.9.3     2023-02-03 [2] CRAN (R 4.3.1)
-# glue                   1.6.2     2022-02-24 [2] CRAN (R 4.3.1)
-# gridExtra              2.3       2017-09-09 [2] CRAN (R 4.3.1)
-# gtable                 0.3.4     2023-08-21 [2] CRAN (R 4.3.1)
-# here                 * 1.0.1     2020-12-13 [2] CRAN (R 4.3.1)
-# igraph                 1.5.1     2023-08-10 [2] CRAN (R 4.3.1)
-# IRanges              * 2.34.1    2023-06-22 [2] Bioconductor
-# irlba                  2.3.5.1   2022-10-03 [2] CRAN (R 4.3.1)
-# labeling               0.4.3     2023-08-29 [2] CRAN (R 4.3.1)
-# lattice                0.21-8    2023-04-05 [3] CRAN (R 4.3.1)
-# lifecycle              1.0.3     2022-10-07 [2] CRAN (R 4.3.1)
-# limma                  3.56.2    2023-06-04 [2] Bioconductor
-# locfit                 1.5-9.8   2023-06-11 [2] CRAN (R 4.3.1)
-# magrittr               2.0.3     2022-03-30 [2] CRAN (R 4.3.1)
-# Matrix                 1.6-1.1   2023-09-18 [3] CRAN (R 4.3.1)
-# MatrixGenerics       * 1.12.3    2023-07-30 [2] Bioconductor
-# matrixStats          * 1.0.0     2023-06-02 [2] CRAN (R 4.3.1)
-# metapod                1.8.0     2023-04-25 [2] Bioconductor
-# munsell                0.5.0     2018-06-12 [2] CRAN (R 4.3.1)
-# pillar                 1.9.0     2023-03-22 [2] CRAN (R 4.3.1)
-# pkgconfig              2.0.3     2019-09-22 [2] CRAN (R 4.3.1)
-# R6                     2.5.1     2021-08-19 [2] CRAN (R 4.3.1)
-# ragg                   1.2.5     2023-01-12 [2] CRAN (R 4.3.1)
-# Rcpp                   1.0.11    2023-07-06 [2] CRAN (R 4.3.1)
-# RcppAnnoy              0.0.21    2023-07-02 [2] CRAN (R 4.3.1)
-# RCurl                  1.98-1.12 2023-03-27 [2] CRAN (R 4.3.1)
-# ResidualMatrix         1.10.0    2023-04-25 [2] Bioconductor
-# rlang                  1.1.1     2023-04-28 [2] CRAN (R 4.3.1)
-# rprojroot              2.0.3     2022-04-02 [2] CRAN (R 4.3.1)
-# rsvd                   1.0.5     2021-04-16 [2] CRAN (R 4.3.1)
-# Rtsne                  0.16      2022-04-17 [2] CRAN (R 4.3.1)
-# S4Arrays               1.0.6     2023-08-30 [2] Bioconductor
-# S4Vectors            * 0.38.1    2023-05-02 [2] Bioconductor
-# ScaledMatrix           1.8.1     2023-05-03 [2] Bioconductor
-# scales                 1.2.1     2022-08-20 [2] CRAN (R 4.3.1)
-# scater               * 1.28.0    2023-04-25 [2] Bioconductor
-# scran                * 1.28.2    2023-07-23 [2] Bioconductor
-# scry                 * 1.12.0    2023-04-25 [2] Bioconductor
-# scuttle              * 1.10.2    2023-08-03 [2] Bioconductor
-# sessioninfo          * 1.2.2     2021-12-06 [2] CRAN (R 4.3.1)
-# SingleCellExperiment * 1.22.0    2023-04-25 [2] Bioconductor
-# sparseMatrixStats      1.12.2    2023-07-02 [2] Bioconductor
-# statmod                1.5.0     2023-01-06 [2] CRAN (R 4.3.1)
-# SummarizedExperiment * 1.30.2    2023-06-06 [2] Bioconductor
-# systemfonts            1.0.4     2022-02-11 [2] CRAN (R 4.3.1)
-# textshaping            0.3.6     2021-10-13 [2] CRAN (R 4.3.1)
-# tibble                 3.2.1     2023-03-20 [2] CRAN (R 4.3.1)
-# tidyselect             1.2.0     2022-10-10 [2] CRAN (R 4.3.1)
-# utf8                   1.2.3     2023-01-31 [2] CRAN (R 4.3.1)
-# uwot                   0.1.16    2023-06-29 [2] CRAN (R 4.3.1)
-# vctrs                  0.6.3     2023-06-14 [2] CRAN (R 4.3.1)
-# vipor                  0.4.5     2017-03-22 [2] CRAN (R 4.3.1)
-# viridis                0.6.4     2023-07-22 [2] CRAN (R 4.3.1)
-# viridisLite            0.4.2     2023-05-02 [2] CRAN (R 4.3.1)
-# withr                  2.5.0     2022-03-03 [2] CRAN (R 4.3.1)
-# XVector                0.40.0    2023-04-25 [2] Bioconductor
-# zlibbioc               1.46.0    2023-04-25 [2] Bioconductor
-# 
-# [1] /users/rphillip/R/4.3
-# [2] /jhpce/shared/community/core/conda_R/4.3/R/lib64/R/site-library
-# [3] /jhpce/shared/community/core/conda_R/4.3/R/lib64/R/library
-# 
-# ────────────────────────────────────────────────────────────────────────────────────
-# 
-# 
