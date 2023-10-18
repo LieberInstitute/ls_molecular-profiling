0aa45684aed29ddd243cb0c3fa79dde244693a6e
Robert Phillips <rphillips11995@gmail.com>
2023-10-18 15:59:39 -0400

    use UMAP_mnn_20 to cluster with several k values

--- code/02_build_sce/05_clustering_and_annotation.R
+++ code/02_build_sce/05_clustering_and_annotation.R
@@ -23,7 +23,7 @@ sce
 # colnames(10000): 1_AAACCCACAGCGTTGC-1 1_AAACCCACATGGCGCT-1 ...
 # 3_TTTGGTTTCTTCGACC-1 3_TTTGTTGTCCCGATCT-1
 # colData names(55): Sample Barcode ... discard_numeric sizeFactor
-# reducedDimNames(5): GLMPCA_approx UMAP TSNE mnn UMAP_mnn
+# reducedDimNames(14): GLMPCA_approx UMAP_15 ... UMAP_mnn_25 UMAP_mnn_50
 # mainExpName: NULL
 # altExpNames(0):
 
@@ -40,10 +40,10 @@ snn_k_75 <- buildSNNGraph(sce, k = 75, use.dimred = "mnn",type="jaccard")
 set.seed(1234)
 clust_20 <- igraph::cluster_louvain(snn_k_20,resolution=1)$membership
 table(clust_20)
-#   1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16 
-# 276 1125  307  200  258 1095  475  542  674  645  225  147  692  143  106  517 
-#  17   18   19   20   21   22   23   24 
-# 252   72   49  168  637  126  310  959
+# 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16 
+# 276 1125  307  200  258 1096  475  542  674  645  225  838  143  106  517  252 
+# 17   18   19   20   21   22   23 
+# 72   49  168  637  126  310  959
 
 set.seed(1234)
 clust_50 <- igraph::cluster_louvain(snn_k_50,resolution=1)$membership
@@ -55,7 +55,7 @@ set.seed(1234)
 clust_75 <- igraph::cluster_louvain(snn_k_75,resolution=1)$membership
 table(clust_75)
 #    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15 
-# 1365 1224  355  193  256 1456  545  650  645 1382  750  214  143  636  186
+# 1365 1224  355  193  256 1456  545  650  645 1382  750  214  143  636  186 
 
 #Add cluster information to object
 sce$k_20_louvain_1 <- factor(clust_20)
@@ -67,50 +67,67 @@ set.seed(1234)
 wt_clusters_k_20 <- igraph::cluster_walktrap(snn_k_20)$membership
 table(wt_clusters_k_20)
 # 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16 
-# 258 1087  301  953  436  379  218 1214  807  851  253  157   61  379  286  307 
+# 258 1087  721  436  375  192  218 1215  807  854  253  338  157   61  379  286 
 # 17   18   19   20   21   22   23   24   25   26   27   28   29   30   31   32 
-# 265  164  113  176   87  170  143  144   58  107   95   77  119   48   54   56 
-# 33   34   35   36 
-# 63   29   43   42 
+# 307  265  164   86  114  179  170  143  144   58  107   95   77  119   48   54 
+# 33   34   35   36   37 
+# 56   63   29   43   42 
 
 set.seed(1234)
 wt_clusters_k_50 <- igraph::cluster_walktrap(snn_k_50)$membership
 table(wt_clusters_k_50)
-# wt_clusters_k_50
+wt_clusters_k_50
 #   1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16 
-# 289  163 1879 1469 1228  643  257  197 1381  283  316  215  332  301  154  149 
+# 289  163 1866 1228  643 1417  257  197 1381  373  316  353  215  332  154  149 
 #  17   18   19   20 
-# 228  258  188   70 
+# 228  271   98   70 
+
+set.seed(1234)
+wt_clusters_k_75 <- igraph::cluster_walktrap(snn_k_75)$membership
+table(wt_clusters_k_75)
+#   1    2    3    4    5    6    7    8    9   10   11   12   13   14 
+# 625  545 1213 2344  646  642  343 1360 1081  255  197  377  230  142 
 
 #Add cluster information to object
 sce$k_20_walktrap <- factor(wt_clusters_k_20)
 sce$k_50_walktrap <- factor(wt_clusters_k_50)
+sce$k_75_walktrap <- factor(wt_clusters_k_75)
 
 #Plot the clusters on the umap
 #k=20 louvain
-k_20_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn",
+k_20_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
                             colour_by = "k_20_louvain_1",text_by = "k_20_louvain_1") #Color and label by cluster 
-ggsave(plot = k_20_umap,filename = here("plots","Dim_Red","k_20_louvain_umap_50components.pdf"))
+ggsave(plot = k_20_umap,filename = here("plots","Dim_Red","k_20_louvain_umap_20components.pdf"))
 
 #k=50 louvain
-k_50_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn",
+k_50_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
                             colour_by = "k_50_louvain_1",text_by = "k_50_louvain_1") #Color and label by cluster 
-ggsave(plot = k_50_umap,filename = here("plots","Dim_Red","k_50_louvain_umap_50components.pdf"))
+ggsave(plot = k_50_umap,filename = here("plots","Dim_Red","k_50_louvain_umap_20components.pdf"))
 
 #k=75 louvain
-k_75_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn",
+k_75_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
                             colour_by = "k_75_louvain_1",text_by = "k_75_louvain_1") #Color and label by cluster 
-ggsave(plot = k_50_umap,filename = here("plots","Dim_Red","k_75_louvain_umap_50components.pdf"))
+ggsave(plot = k_50_umap,filename = here("plots","Dim_Red","k_75_louvain_umap_20components.pdf"))
 
 #k=20 walktrap
-k_20_wt_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn",
+k_20_wt_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
                                colour_by = "k_20_walktrap",text_by = "k_20_walktrap") #Color and label by cluster 
-ggsave(plot = k_20_wt_umap,filename = here("plots","Dim_Red","k_20_walktrap_umap_50components.pdf"))
+ggsave(plot = k_20_wt_umap,filename = here("plots","Dim_Red","k_20_walktrap_umap_20components.pdf"))
 
 #k=50 walktrap
-k_50_wt_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn",
+k_50_wt_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
                                colour_by = "k_50_walktrap",text_by = "k_50_walktrap") #Color and label by cluster 
-ggsave(plot = k_50_wt_umap,filename = here("plots","Dim_Red","k_50_walktrap_umap_50components.pdf"))
+ggsave(plot = k_50_wt_umap,filename = here("plots","Dim_Red","k_50_walktrap_umap_20components.pdf"))
+
+#k=50 walktrap
+k_50_wt_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
+                               colour_by = "k_50_walktrap",text_by = "k_50_walktrap") #Color and label by cluster 
+ggsave(plot = k_50_wt_umap,filename = here("plots","Dim_Red","k_50_walktrap_umap_20components.pdf"))
+
+#k=75 walktrap
+k_75_wt_umap <- plotReducedDim(object = sce,dimred = "UMAP_mnn_20",
+                               colour_by = "k_75_walktrap",text_by = "k_75_walktrap") #Color and label by cluster 
+ggsave(plot = k_75_wt_umap,filename = here("plots","Dim_Red","k_75_wt_umap_umap_20components.pdf"))
 
 #Tables by cluster and sample
 table(sce$k_20_louvain_1,sce$Sample)
@@ -120,28 +137,27 @@ table(sce$k_20_louvain_1,sce$Sample)
 # 3        307         0         0
 # 4         57        80        63
 # 5        152        75        31
-# 6        550       493        52
+# 6        550       493        53
 # 7         38       284       153
 # 8        321       196        25
 # 9        295       281        98
 # 10       472       102        71
 # 11       158        29        38
-# 12        48        21        78
-# 13       211       214       267
-# 14        59        31        53
-# 15        62        39         5
-# 16       257       131       129
-# 17        83        57       112
-# 18        72         0         0
-# 19        39        10         0
-# 20       109        27        32
-# 21       234       263       140
-# 22        96         4        26
-# 23        20       103       187
-# 24         5       301       653
+# 12       259       235       344
+# 13        59        31        53
+# 14        62        39         5
+# 15       257       131       129
+# 16        83        57       112
+# 17        72         0         0
+# 18        39        10         0
+# 19       109        27        32
+# 20       234       263       140
+# 21        96         4        26
+# 22        20       103       187
+# 23         5       301       653
 
 table(sce$k_50_louvain_1,sce$Sample)
-#     1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
+#    1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
 # 1        753       608        69
 # 2        646        35       444
 # 3        362         0         0
@@ -178,71 +194,89 @@ table(sce$k_75_louvain_1,sce$Sample)
 # 15         3        44       139
 
 table(sce$k_20_walktrap,sce$Sample)
-#    1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
+#     1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
 # 1        152        75        31
 # 2        544       492        51
-# 3         19        99       183
-# 4        626        34       293
-# 5        321        94        21
-# 6        134       173        72
+# 3        468        88       165
+# 4        321        94        21
+# 5        132       171        72
+# 6          2        45       145
 # 7        154        27        37
-# 8        464       347       403
+# 8        465       347       403
 # 9          5       288       514
-# 10       150       474       227
+# 10       151       476       227
 # 11        83        57       113
-# 12        75        47        35
-# 13        36        25         0
-# 14       242        88        49
-# 15       174        99        13
-# 16       183        95        29
-# 17       229        14        22
-# 18       106        26        32
-# 19         5        53        55
-# 20        24         0       152
-# 21        52        27         8
-# 22       170         0         0
-# 23         0         9       134
-# 24        48        19        77
-# 25        55         3         0
-# 26         0       103         4
-# 27        20        43        32
-# 28        13        16        48
-# 29       119         0         0
-# 30        38        10         0
-# 31        50         4         0
-# 32        38        14         4
-# 33        37         1        25
-# 34        29         0         0
-# 35        24        14         5
-# 36        42         0         0
+# 12       175         0       163
+# 13        75        47        35
+# 14        36        25         0
+# 15       242        88        49
+# 16       174        99        13
+# 17       183        95        29
+# 18       229        14        22
+# 19       106        26        32
+# 20        52        26         8
+# 21         5        54        55
+# 22        24         0       155
+# 23       170         0         0
+# 24         0         9       134
+# 25        48        19        77
+# 26        55         3         0
+# 27         0       103         4
+# 28        20        43        32
+# 29        13        16        48
+# 30       119         0         0
+# 31        38        10         0
+# 32        50         4         0
+# 33        38        14         4
+# 34        37         1        25
+# 35        29         0         0
+# 36        24        14         5 
 
 table(sce$k_50_walktrap,sce$Sample)
-#    1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
+#     1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
 # 1        289         0         0
 # 2         95        38        30
-# 3        724       468       687
-# 4        158       715       596
-# 5        661        85       482
-# 6        470       102        71
+# 3        719       466       681
+# 4        661        85       482
+# 5        470       102        71
+# 6        137       692       588
 # 7        151        75        31
 # 8         55        79        63
 # 9        738       585        58
-# 10        86       143        54
+# 10       124       176        73
 # 11       126       170        20
-# 12       155        27        33
-# 13         0        54       278
-# 14       180        92        29
+# 12       201       115        37
+# 13       155        27        33
+# 14         0        54       278
 # 15        73        47        34
 # 16        92        26        31
 # 17       196        27         5
-# 18        83        60       115
-# 19        59        77        52
+# 18        88        62       121
+# 19        21        44        33
 # 20        70         0         0
+
+table(sce$k_75_walktrap,sce$Sample)
+#     1c_LS_SCP 2c_LS_SCP 3c_LS_SCP
+# 1        220       267       138
+# 2        323       197        25
+# 3        658        79       476
+# 4        922       583       839
+# 5        472       103        71
+# 6        289       263        90
+# 7        343         0         0
+# 8        736       572        52
+# 9         47       534       500
+# 10       149        75        31
+# 11        55        79        63
+# 12         0        64       313
+# 13       162        28        40
+# 14        85        26        31 
+
 #Preliminary explorration of distribution of expression of marker genes found that one cluster 
 #was dominated by Slc17a7 expression and only contained sample 1. This is not driven by batch effect
 #but rather biological (due to anatomy of section?). k=20 seems to contain several clusters dominated by one 
-#sample, while k=50 only has one cluster that is dominated by sample 1 (and is the cluster identified yesterday)
-#going to check expression profiles of k=50 louvain. 
+#sample, while k=50 only has one cluster that is dominated by sample 1 (and is the cluster identified yesterday).
+#k=75 looks best here. 
 
 #save the object
 save(sce,file = here("processed-data","sce_clustered.rda"))
