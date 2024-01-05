97424cff434a51a589044cdd8b95da6256fbbdcc
Robert Phillips <rphillips11995@gmail.com>
2024-01-05 11:04:09 -0500

    Rerun with subsetted mouse sce

--- code/03_cross_species/humanLS_vs_mouseLS_MNN.R
+++ code/03_cross_species/humanLS_vs_mouseLS_MNN.R
@@ -5,6 +5,7 @@
 #cd /dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling
 
 library(SingleCellExperiment)
+library(sessioninfo)
 library(batchelor)
 library(harmony)
 library(scater)
@@ -13,31 +14,40 @@ library(scry)
 
 #Load the human and mouse matched SCE objects.
 load(here("processed-data","human_mouse_matched_by_JAX.rda"),verbose = TRUE)
-# Loading objects:
-#     sce_mouse_sub
-#     sce_human_sub
 
 sce_mouse_sub
 # class: SingleCellExperiment 
-# dim: 16572 22860 
+# dim: 16562 21884 
 # metadata(1): Samples
 # assays(3): counts binomial_pearson_residuals logcounts
-# rownames(16572): ENSMUSG00000096351 ENSMUSG00000095567 ...
+# rownames(16562): ENSMUSG00000096351 ENSMUSG00000095567 ...
 # ENSMUSG00000037772 ENSMUSG00000003526
 # rowData names(9): source type ... mm.entrezIds JAX.geneID
-# colnames(22860): 1_AAACCCAAGGTACATA-1 1_AAACCCACATCCGAGC-1 ...
+# colnames(21884): 1_AAACCCAAGGTACATA-1 1_AAACCCACATCCGAGC-1 ...
 # 4_TTTGTTGCATACAGCT-1 4_TTTGTTGGTCAAACGG-1
 # colData names(17): Sample Barcode ... cellType.final cellType.broad
 # reducedDimNames(4): GLMPCA_approx UMAP TSNE GLMPCA_50
 # mainExpName: NULL
 # altExpNames(0):
 
+#Make sure the mixed and doublet populations are gone. 
+levels(sce_mouse_sub$cellType.final)
+# [1] "Astro"         "Chol_Ex.D"     "ChP"           "Endo"         
+# [5] "Ependymal"     "IoC_In.E"      "LS_In.C"       "LS_In.D"      
+# [9] "LS_In.M"       "LS_In.N"       "LS_In.O"       "LS_In.P"      
+# [13] "LS_In.Q"       "LS_In.R"       "Micro"         "MS_In.J"      
+# [17] "MS_In.K"       "Mural"         "Neuroblast"    "Oligo"        
+# [21] "OPC"           "OPC_COP"       "Sept_In.G"     "Sept_In.I"    
+# [25] "Str_In.A"      "Str_In.F"      "Str_In.H"      "Str_In.L"     
+# [29] "Thal_Ex.B"     "TNoS_Ex.A"     "TT.IG.SH_Ex.C" "TT.IG.SH_Ex.E"
+# [33] "TT.IG.SH_Ex.F"
+
 sce_human_sub
 # class: SingleCellExperiment 
-# dim: 16572 9225 
+# dim: 16562 9225 
 # metadata(1): Samples
 # assays(3): counts binomial_deviance_residuals logcounts
-# rownames(16572): ENSG00000187634 ENSG00000188976 ... ENSG00000214026
+# rownames(16562): ENSG00000187634 ENSG00000188976 ... ENSG00000214026
 # ENSG00000100033
 # rowData names(9): source type ... hs.entrezIds JAX.geneID
 # colnames(9225): 1_AAACCCACAGCGTTGC-1 1_AAACCCACATGGCGCT-1 ...
@@ -76,13 +86,13 @@ sce_combo <- SingleCellExperiment(assays=list(counts=combo_counts), colData=comb
 
 sce_combo
 # class: SingleCellExperiment 
-# dim: 16572 32085 
+# dim: 16562 31109 
 # metadata(0):
 #     assays(1): counts
-# rownames(16572): ENSG00000187634 ENSG00000188976 ... ENSG00000214026
+# rownames(16562): ENSG00000187634 ENSG00000188976 ... ENSG00000214026
 # ENSG00000100033
 # rowData names(0):
-#     colnames(32085): 1_AAACCCACAGCGTTGC-1 1_AAACCCACATGGCGCT-1 ...
+#     colnames(31109): 1_AAACCCACAGCGTTGC-1 1_AAACCCACATGGCGCT-1 ...
 # 4_TTTGTTGCATACAGCT-1 4_TTTGTTGGTCAAACGG-1
 # colData names(6): Sample Barcode ... detected CellType
 # reducedDimNames(0):
@@ -222,7 +232,8 @@ umap_cross_species_ct <- plotReducedDim(sce_combo,
                                         colour_by = "CellType_Species",
                                         text_by = "CellType_Species",
                                         point_alpha = 0.3) +
-    ggplot2::theme(legend.position = "none")
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::scale_color_manual(values = cluster_cols)
 ggsave(umap_cross_species_ct,filename = here("plots",
                                              "Conservation",
                                              "umap_cross_species_mnn_corrected_byCellType.png"),
@@ -234,7 +245,8 @@ tSNE_cross_species_ct <- plotReducedDim(sce_combo,
                                         colour_by = "CellType_Species",
                                         text_by = "CellType_Species",
                                         point_alpha = 0.3) +
-    ggplot2::theme(legend.position = "none")
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::scale_color_manual(values = cluster_cols)
 ggsave(tSNE_cross_species_ct,filename = here("plots",
                                              "Conservation",
                                              "tSNE_cross_species_mnn_corrected_byCellType.png"),
@@ -292,8 +304,116 @@ ggsave(tSNE_Harmony,filename = here("plots",
 
 save(sce_harmony_Species,file = here("processed-data","sce_harmony_species.rda"))
 
-#save the object with harmony. 
-#Harmony looks good. Rename the object and then
-
-
-
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+# [1] "Reproducibility information:"
+# [1] "2024-01-05 11:01:43 EST"
+#     user   system  elapsed 
+# 1763.269   47.078 4251.536
+# ─ Session info ──────────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.3.1 Patched (2023-07-19 r84711)
+# os       Rocky Linux 9.2 (Blue Onyx)
+# system   x86_64, linux-gnu
+# ui       X11
+# language (EN)
+# collate  en_US.UTF-8
+# ctype    en_US.UTF-8
+# tz       US/Eastern
+# date     2024-01-05
+# pandoc   3.1.3 @ /jhpce/shared/community/core/conda_R/4.3/bin/pandoc
+# 
+# ─ Packages ──────────────────────────────────────────────────────────────────────
+# package              * version   date (UTC) lib source
+# abind                  1.4-5     2016-07-21 [2] CRAN (R 4.3.1)
+# batchelor            * 1.16.0    2023-04-25 [2] Bioconductor
+# beachmat               2.16.0    2023-04-25 [2] Bioconductor
+# beeswarm               0.4.0     2021-06-01 [2] CRAN (R 4.3.1)
+# Biobase              * 2.60.0    2023-04-25 [2] Bioconductor
+# BiocGenerics         * 0.46.0    2023-04-25 [2] Bioconductor
+# BiocNeighbors          1.18.0    2023-04-25 [2] Bioconductor
+# BiocParallel           1.34.2    2023-05-22 [2] Bioconductor
+# BiocSingular           1.16.0    2023-04-25 [2] Bioconductor
+# bitops                 1.0-7     2021-04-24 [2] CRAN (R 4.3.1)
+# cli                    3.6.1     2023-03-23 [2] CRAN (R 4.3.1)
+# codetools              0.2-19    2023-02-01 [3] CRAN (R 4.3.1)
+# colorout             * 1.3-0.1   2023-12-01 [1] Github (jalvesaq/colorout@deda341)
+# colorspace             2.1-0     2023-01-23 [2] CRAN (R 4.3.1)
+# cowplot                1.1.1     2020-12-30 [2] CRAN (R 4.3.1)
+# crayon                 1.5.2     2022-09-29 [2] CRAN (R 4.3.1)
+# DelayedArray           0.26.7    2023-07-28 [2] Bioconductor
+# DelayedMatrixStats     1.22.6    2023-08-28 [2] Bioconductor
+# dplyr                  1.1.3     2023-09-03 [2] CRAN (R 4.3.1)
+# fansi                  1.0.4     2023-01-22 [2] CRAN (R 4.3.1)
+# farver                 2.1.1     2022-07-06 [2] CRAN (R 4.3.1)
+# generics               0.1.3     2022-07-05 [2] CRAN (R 4.3.1)
+# GenomeInfoDb         * 1.36.3    2023-09-07 [2] Bioconductor
+# GenomeInfoDbData       1.2.10    2023-07-20 [2] Bioconductor
+# GenomicRanges        * 1.52.0    2023-04-25 [2] Bioconductor
+# ggbeeswarm             0.7.2     2023-04-29 [2] CRAN (R 4.3.1)
+# ggplot2              * 3.4.3     2023-08-14 [2] CRAN (R 4.3.1)
+# ggrepel                0.9.3     2023-02-03 [2] CRAN (R 4.3.1)
+# glue                   1.6.2     2022-02-24 [2] CRAN (R 4.3.1)
+# gridExtra              2.3       2017-09-09 [2] CRAN (R 4.3.1)
+# gtable                 0.3.4     2023-08-21 [2] CRAN (R 4.3.1)
+# harmony              * 1.0.1     2023-09-20 [2] CRAN (R 4.3.1)
+# here                 * 1.0.1     2020-12-13 [2] CRAN (R 4.3.1)
+# igraph                 1.5.1     2023-08-10 [2] CRAN (R 4.3.1)
+# IRanges              * 2.34.1    2023-06-22 [2] Bioconductor
+# irlba                  2.3.5.1   2022-10-03 [2] CRAN (R 4.3.1)
+# labeling               0.4.3     2023-08-29 [2] CRAN (R 4.3.1)
+# lattice                0.21-8    2023-04-05 [3] CRAN (R 4.3.1)
+# lifecycle              1.0.3     2022-10-07 [2] CRAN (R 4.3.1)
+# magrittr               2.0.3     2022-03-30 [2] CRAN (R 4.3.1)
+# Matrix                 1.6-1.1   2023-09-18 [3] CRAN (R 4.3.1)
+# MatrixGenerics       * 1.12.3    2023-07-30 [2] Bioconductor
+# matrixStats          * 1.0.0     2023-06-02 [2] CRAN (R 4.3.1)
+# munsell                0.5.0     2018-06-12 [2] CRAN (R 4.3.1)
+# pillar                 1.9.0     2023-03-22 [2] CRAN (R 4.3.1)
+# pkgconfig              2.0.3     2019-09-22 [2] CRAN (R 4.3.1)
+# Polychrome             1.5.1     2022-05-03 [1] CRAN (R 4.3.1)
+# R6                     2.5.1     2021-08-19 [2] CRAN (R 4.3.1)
+# ragg                   1.2.5     2023-01-12 [2] CRAN (R 4.3.1)
+# Rcpp                 * 1.0.11    2023-07-06 [2] CRAN (R 4.3.1)
+# RcppAnnoy              0.0.21    2023-07-02 [2] CRAN (R 4.3.1)
+# RCurl                  1.98-1.12 2023-03-27 [2] CRAN (R 4.3.1)
+# ResidualMatrix         1.10.0    2023-04-25 [2] Bioconductor
+# RhpcBLASctl            0.23-42   2023-02-11 [2] CRAN (R 4.3.1)
+# rlang                  1.1.1     2023-04-28 [2] CRAN (R 4.3.1)
+# rprojroot              2.0.3     2022-04-02 [2] CRAN (R 4.3.1)
+# rsvd                   1.0.5     2021-04-16 [2] CRAN (R 4.3.1)
+# Rtsne                  0.16      2022-04-17 [2] CRAN (R 4.3.1)
+# S4Arrays               1.0.6     2023-08-30 [2] Bioconductor
+# S4Vectors            * 0.38.1    2023-05-02 [2] Bioconductor
+# ScaledMatrix           1.8.1     2023-05-03 [2] Bioconductor
+# scales                 1.2.1     2022-08-20 [2] CRAN (R 4.3.1)
+# scater               * 1.28.0    2023-04-25 [2] Bioconductor
+# scatterplot3d          0.3-44    2023-05-05 [1] CRAN (R 4.3.1)
+# scry                 * 1.12.0    2023-04-25 [2] Bioconductor
+# scuttle              * 1.10.2    2023-08-03 [2] Bioconductor
+# sessioninfo          * 1.2.2     2021-12-06 [2] CRAN (R 4.3.1)
+# SingleCellExperiment * 1.22.0    2023-04-25 [2] Bioconductor
+# sparseMatrixStats      1.12.2    2023-07-02 [2] Bioconductor
+# SummarizedExperiment * 1.30.2    2023-06-06 [2] Bioconductor
+# systemfonts            1.0.4     2022-02-11 [2] CRAN (R 4.3.1)
+# textshaping            0.3.6     2021-10-13 [2] CRAN (R 4.3.1)
+# tibble                 3.2.1     2023-03-20 [2] CRAN (R 4.3.1)
+# tidyselect             1.2.0     2022-10-10 [2] CRAN (R 4.3.1)
+# utf8                   1.2.3     2023-01-31 [2] CRAN (R 4.3.1)
+# uwot                   0.1.16    2023-06-29 [2] CRAN (R 4.3.1)
+# vctrs                  0.6.3     2023-06-14 [2] CRAN (R 4.3.1)
+# vipor                  0.4.5     2017-03-22 [2] CRAN (R 4.3.1)
+# viridis                0.6.4     2023-07-22 [2] CRAN (R 4.3.1)
+# viridisLite            0.4.2     2023-05-02 [2] CRAN (R 4.3.1)
+# withr                  2.5.0     2022-03-03 [2] CRAN (R 4.3.1)
+# XVector                0.40.0    2023-04-25 [2] Bioconductor
+# zlibbioc               1.46.0    2023-04-25 [2] Bioconductor
+# 
+# [1] /users/rphillip/R/4.3
+# [2] /jhpce/shared/community/core/conda_R/4.3/R/lib64/R/site-library
+# [3] /jhpce/shared/community/core/conda_R/4.3/R/lib64/R/library
+# 
+# ─────────────────────────────────────────────────────────────────────────────────
