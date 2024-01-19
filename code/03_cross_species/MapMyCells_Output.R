abee12ed478240e8900aea2cd974a7a4bbd8e204
Robert Phillips <rphillips11995@gmail.com>
2024-01-19 15:08:36 -0500

    bargraph for LS neurons to mapped mouse classes

--- code/03_cross_species/MapMyCells_Output.R
+++ code/03_cross_species/MapMyCells_Output.R
@@ -26,7 +26,27 @@ hi_output <- merge(x = colData(sce)[,c("unique_rowname","CellType.Final")],
                    by.x = "unique_rowname",
                    by.y = "cell_id")
 
+#Bargraph to see where each of the LS clusters are mapping to
+LS_cells <- hi_output[grep("LS",hi_output$CellType.Final),]
 
+#Create an empty dataframe
+LS_cells_props <- data.frame(CellType.Final  = NA,
+                             Mapped_CellType = NA,
+                             Freq = NA,
+                             Prop = NA)
+
+for(i in unique(LS_cells$CellType.Final)){
+    prop_df <- as.data.frame(table(subset(LS_cells,subset=(CellType.Final == i))$class_name))
+    prop_df$CellType.Final <- i
+    prop_df$Prop <- prop_df$Freq/sum(prop_df$Freq)*100
+    colnames(prop_df)[1] <- "Mapped_CellType"
+    LS_cells_props <- rbind(LS_cells_props,prop_df[,c("CellType.Final","Mapped_CellType","Freq","Prop")])
+}
+
+LS_cells_props <- LS_cells_props[!is.na(LS_cells_props$CellType.Final),]
+
+ggplot(data = LS_cells_props,aes(x = CellType.Final,y = Prop, fill = Mapped_CellType)) +
+    geom_bar(stat = "identity")
 
 
 
