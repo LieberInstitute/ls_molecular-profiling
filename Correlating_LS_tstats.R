e7bfada941fa21f466d22566f4158cf261f5b884
Robert Phillips <rphillips11995@gmail.com>
2024-01-22 15:01:02 -0500

    calculate t stats for each species

--- Correlating_LS_tstats.R
+++ Correlating_LS_tstats.R
@@ -52,6 +52,9 @@ h_LS_DEGs <- dplyr::left_join(x = as.data.frame(h_LS_DEGs),
                               y = as.data.frame(rowData(sce)[,c("gene_id","gene_name")]),
                               by = "gene_id")
 
+#Calcualte t statistics for human
+h_LS_DEGs$t.stat <- h_LS_DEGs$std.logFC * sqrt(ncol(sce))
+
 ##load the SingleCellExperiment object for mouse Lateral Septum
 load(file = "/dcs04/lieber/marmaypag/pilotLS_LIBD1070/snRNAseq_mouse/processed_data/SCE/sce_updated_LS.rda",verbose = TRUE)
 # Loading objects:
@@ -98,14 +101,27 @@ m_LS_DEGs <- dplyr::left_join(x = as.data.frame(m_LS_DEGs),
                               y = as.data.frame(rowData(sce.ls)[,c("gene_id","gene_name")]),
                               by = "gene_id")
 
+#Calculate t statistic for mouse data
+m_LS_DEGs$t.stat <- m_LS_DEGs$std.logFC * sqrt(ncol(sce.ls))
+
 
 #Load the human and mouse matched SCE objects.
 load(here("processed-data","human_mouse_matched_by_JAX.rda"),verbose = TRUE)
 # Loading objects:
 #     sce_mouse_sub
 #     sce_human_sub
+#The objects above are subsetted to contain only genes that are homologous between the two species
 
+#Add Jax.GeneID info to the human LS DEGs. 
+#This also removes any genes from the DEG table that are not homologous
+h_LS_DEGs_homol <- merge(h_LS_DEGs,
+                         rowData(sce_human_sub)[,c("gene_id","gene_name","JAX.geneID")],
+                         by = "gene_id") 
 
+#Add Jax.GeneID info to the mouse LS DEGs and also remove any genes that are not homologus
+m_LS_DEGs_homol <- merge(m_LS_DEGs,
+                         rowData(sce_mouse_sub)[,c("gene_id","gene_name","JAX.geneID")],
+                         by = "gene_id") 
 
 
 
