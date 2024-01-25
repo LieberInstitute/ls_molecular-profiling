78a73b3458114ba1196c86c569aec6416fc9cd01
Rphillips1995 <Rphillips11995@gmail.com>
2024-01-25 14:03:35 -0500

    organize files for cross-species analysis

---
+++ code/03_cross_species/shuffle_mouse_t_stat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="Mouse stat shuffling"
+#SBATCH --mem=10GB
+#SBATCH --output=mouse_shuffle.out
+#SBATCH --error=mouse_shuffle.err
+
+echo "`date`: loading modules"
+module use /jhpce/shared/libd/modulefiles
+module load conda_R/4.3
+
+cd /dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/
+
+echo "`date`: Running R Job"
+R CMD BATCH shuffle_mouse_t_stats.R
+
+echo "`date`: R job complete"
