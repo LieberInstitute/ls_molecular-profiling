5b4e89f1e193bee3b13ba40082966e72f68b1e0d
Rphillips1995 <Rphillips11995@gmail.com>
2024-03-28 14:31:50 -0400

    change rownames to gene symbol to avoid searching by ensembl id

--- code/04_iSEE_app/app.R
+++ code/04_iSEE_app/app.R
@@ -5,29 +5,18 @@ library("paletteer")
 library("scuttle")
 library("SpatialExperiment")
 
+#Load the object
 load("sce_clean.rda", verbose = TRUE)
 
-## Don't run this on app.R since we don't want to run this every single time
-#lobstr::obj_size(spe_pseudo)
+#Change the rownames frome ensembl id to gene_name
+rownames(sce) <-  uniquifyFeatureNames(rowData(sce)$gene_id, rowData(sce)$gene_name)
 
-
-
-
+#Source
 source("initial.R", print.eval = TRUE)
 
-
-#rse_gene <- registerAppOptions(rse_gene, color.maxlevels = length(Sample_ID)
+#Deploy app
 iSEE(
-  spe_pseudo,
+  sce,
   appTitle = "LS snRNAseq data",
-  initial = initial,
-  colormap = ExperimentColorMap(colData = list(
-    domain = function(n) {
-      cols <- spatial.palette
-    },
-    broad.domain = function(n) {
-      cols <- spatial.palette2
-    }
-    
-  ))
+  initial = initial
 )
