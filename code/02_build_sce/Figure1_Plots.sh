3e9d2e2af38894cdb03c891e4a158d4ba2b42c38
Rphillips1995 <Rphillips11995@gmail.com>
2024-08-26 15:59:40 -0400

    job script for Figure1_Plots.R

---
+++ code/02_build_sce/Figure1_Plots.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Fig1_Plots
+#SBATCH --output=logs/Fig1_Plots.log
+#SBATCH --error=logs/Fig1_Plots.log
+#
+# Number of CPUs allocated to each task.
+#SBATCH --cpus-per-task=1
+#
+# Mimimum memory required per allocated  CPU
+#SBATCH --mem-per-cpu=5G
+#
+# Send mail to the email address when the job fails
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robert.phillips@libd.org
+
+
+echo "********* Job Starts *********"
+date
+
+#load R
+module load conda_R/4.4
+
+#list modules for reproducibility purposes
+module list
+
+#run the Rjob
+Rscript Figure1_Plots.R
+
+echo "********* Job Ends *********"
+date
