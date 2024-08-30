069872e8e30a57004ef34ca35250c0e1d6011770
Rphillips1995 <Rphillips11995@gmail.com>
2024-08-30 13:59:47 -0400

    initalize script for 1vALL DEG testing with mouse LS object

---
+++ code/03_cross_species/mouse_LS_1vALL_DEGs.R
@@ -0,0 +1,6 @@
+#Goal: Perform 1vALL DEG testing with DeconvoBuddies as was done with human dataset.
+#cd /dcs04/lieber/marmaypag/ls_molecular-profiling_LIBD1070/ls_molecular-profiling/
+
+library(SingleCellExperiment)
+library(DeconvoBuddies)
+library(here)
