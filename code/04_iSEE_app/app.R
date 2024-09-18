76701d4724f67438e2129e940567b8afdf779cfd
Rphillips1995 <Rphillips11995@gmail.com>
2024-09-18 16:20:18 -0400

    update app script with new symlink

--- code/04_iSEE_app/app.R
+++ code/04_iSEE_app/app.R
@@ -6,7 +6,7 @@ library("scuttle")
 library("SpatialExperiment")
 
 #Load the object
-load("sce_celltype.rda", verbose = TRUE)
+load("sce_app.rda", verbose = TRUE)
 
 #Change the rownames frome ensembl id to gene_name
 rownames(sce) <-  uniquifyFeatureNames(rowData(sce)$gene_id, rowData(sce)$gene_name)
